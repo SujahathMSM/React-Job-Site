ef17a1e5f810e10402b458a1a5063e9a191683e1
sujahathmsm <sujahathmhmd@gmail.com>
2024-07-17 10:28:21 +0530

    Added Spinner and Proxy

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import JobsList from './JobsList'
 import { useState, useEffect } from 'react'
+import Spinner from './Spinner';
 // eslint-disable-next-line react/prop-types
 const JobListings = ({ Home = false }) => {
 
@@ -10,9 +11,9 @@ const JobListings = ({ Home = false }) => {
 
   useEffect(() => {
     const fetchJobs = async () => {
-
+      const apiUrl = Home ? '/api/jobs?_limit=3' : '/api/jobs';
       try {
-        const res = await fetch('http://localhost:5000/jobs')
+        const res = await fetch(apiUrl)
         const data = await res.json();
         setJobs(data)
       } catch (error) {
@@ -26,23 +27,24 @@ const JobListings = ({ Home = false }) => {
     fetchJobs();
   }, [])
 
+  // const ajobs =  Home ? jobs.slice(0, 3) : jobs;
+
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
           {Home ? "Recent Jobs" : "Browse Jobs"}
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
 
-          {loading ? (<h2>Loading ...</h2>) : (
-            <>
-              {jobs.map((job) => (
-                <JobsList Job={job} key={job.id} />
-              ))}
-            </>
-          )}
+        {loading ? (<Spinner loading={loading} />) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {jobs.map((job) => (
+              <JobsList Job={job} key={job.id} />
+            ))}
+          </div>
+        )}
 
-        </div>
+
       </div>
     </section>
   )
