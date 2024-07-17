91989bc3768364f1dd5eab6f9cd450445ab667c8
sujahathmsm <sujahathmhmd@gmail.com>
2024-07-17 09:56:39 +0530

    Added the json file as a server

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,26 +1,50 @@
+/* This code snippet is a React component named `JobListings`. Here's a breakdown of what it does: */
 import React from 'react'
-import Jobs from '../jobs.json'
 import JobsList from './JobsList'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 // eslint-disable-next-line react/prop-types
-const JobListings = ({Home=false}) => {
+const JobListings = ({ Home = false }) => {
 
-  const recentJobs = Home ? Jobs.slice(0,3): Jobs;
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+
+      try {
+        const res = await fetch('http://localhost:5000/jobs')
+        const data = await res.json();
+        setJobs(data)
+      } catch (error) {
+        console.log("Error Fecthing data", error);
+      } finally {
+        setLoading(false)
+      }
+
+    }
+
+    fetchJobs();
+  }, [])
+
   return (
     <section className="bg-blue-50 px-4 py-10">
-    <div className="container-xl lg:container m-auto">
-      <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-        { Home ? "Recent Jobs" : "Browse Jobs"}
-      </h2>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+      <div className="container-xl lg:container m-auto">
+        <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+          {Home ? "Recent Jobs" : "Browse Jobs"}
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
 
-      {recentJobs.map((job) => (
-        <JobsList Job={job} key={job.id}/>
-      ))}
-        
+          {loading ? (<h2>Loading ...</h2>) : (
+            <>
+              {jobs.map((job) => (
+                <JobsList Job={job} key={job.id} />
+              ))}
+            </>
+          )}
+
+        </div>
       </div>
-    </div>
-  </section>
+    </section>
   )
 }
 
